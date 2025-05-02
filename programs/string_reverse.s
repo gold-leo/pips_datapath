c5cb4820f2ee0a1a3580d09b71649a6f5c22494f
AlexisM345 <alexisjmonteros@gmail.com>
2025-05-01 20:34:37 -0500

    Updated rules + string_reverse

--- programs/string_reverse.s
+++ programs/string_reverse.s
@@ -1,4 +1,5 @@
 # 'string_reverse' takes input from the keyboard and reverses what was inputted.
+# Credit to Charlie Curtsinger for the code to input and output via keyboard and terminal respecitvely.
 .constant HALT      0xff00
 .constant TERM      0xff10
 .constant KBD       0xff20
@@ -9,17 +10,29 @@ main:
   li    $s0, TERM             # Address of the terminal output for memory-mapped I/O
   li    $s1, KBD              # Address of the keyboard input for memory-mapped I/O
   li    $sp, STACK_TOP        # Predefined initial stack address
+  jal   take_string           # Call take-string
+  j     HALT                  # Enable halt pin and stop the PC incrementing
 
+# Takes characters from a keyboard input until a newline character (\n) is inputted
+# No Preconditions
+# Postconditions: The stack contains all return addresses and characters 
+#                 inputted expect the newline character.
 take_string:
   lb    $t0, 0($s1)           # Load a character from the keyboard
-  beq   $t0, $zero, loop_top  # If it is the null terminator, start over
-  addi  $sp, $sp, 3
-  sb    $ra, 0($sp)   
-  sb    $t0, 2($sp)           # Write the character to the stack
   li    $t1, '\n'             # Load a newline constant
+  beq   $t0, $zero, take_string  # If it is the null terminator, start over
   beq   $t0, $t1, string_rev  # If the character was the newline, branch to 'string_rev'
-  jal   take_string           # Start over  
-
+  addi  $sp, $sp, -4          # Allocate stack
+  sw    $ra, 0($sp)           # Store return address to stack
+  sb    $t0, 2($sp)           # Write the character to the stack
+  jal   take_string           # Start over 
+   
+# Takes characters from the stack and prints them in reverse order to the terminal.
+# No preconditions. 
+# Postconditions: Terminal prints characters in reverse order.
 string_rev:
-  lb    $    
-  j   HALT                    # Enable halt pin and stop the PC incrementing
+  lb    $t0, 2($sp)           # Retrieves return address from the stack
+  lw    $ra, 0($sp)           # Retrieves most recent character from the stack
+  sb    $t0, 0($s0)           # Write the character to the terminal
+  addi  $sp, $sp, 4           # Deallocate stack
+  jr    $ra                   # Return to caller
