20c97d2dc3b4efad039c0a4434739c82c4334df6
gold-leo <168613134+gold-leo@users.noreply.github.com>
2025-05-01 19:04:39 -0500

    Added string_uppercase.s

---
+++ programs/string_uppercase.s
@@ -0,0 +1,27 @@
+# Turn any types string into uppercase.
+# Credit to Charlie Curtsinger for partial code.
+.constant TERM      0xff10
+.constant HALT      0xff00
+.constant KBD       0xff20
+.constant STACK_TOP 0xf800
+
+nop
+main:
+  li   $s0, TERM       # Address of the terminal output for memory-mapped I/O
+  li   $s1, KBD        # Address of the keyboard input for memory-mapped I/O
+
+loop:
+  lb   $t0, 0($s1)           # Load a character from the keyboard
+  beq  $t0, $zero, loop      # If it is the null terminator, start over
+  subi $t1, $t0, 97          # Subtract for ASCII comparison
+  sltu $t2, $t1, 26          # Compare if $t1 is a lowercase letter
+  beq  $t2, $zero, skip      # If $t1 is lowercase, don't make uppercase
+  addi $t1, $t1, 65          # Convert to uppercase
+skip:
+  sb   $t0, 0($s0)           # Write the character to the terminal
+  li   $t1, '\n'             # Load a newline constant
+  beq  $t0, $t1, end         # If the character was the newline, stop looping
+  j    loop_top              # Start over
+
+end:
+  j    HALT                  # Enable halt pin and stop the PC incrementing  lb  $t0, 0($s1)
